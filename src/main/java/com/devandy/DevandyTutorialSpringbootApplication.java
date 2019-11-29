0c5fa3f4e4b807f3147aacd807fb8160f553c05f
DevAndy <creator.youngjin@gmail.com>
2019-11-29 15:03:54 +0900

    Started project

---
+++ src/main/java/com/devandy/DevandyTutorialSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.devandy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevandyTutorialSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevandyTutorialSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devandy/DevandyTutorialSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devandy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevandyTutorialSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
