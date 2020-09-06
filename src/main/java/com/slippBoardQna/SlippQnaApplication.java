492ac4d272a7adc3a9a4f25d55a44b8159403b69
youngjinmo <dev.youngjinmo@gmail.com>
2020-09-06 21:36:16 +0900

    init project

---
+++ src/main/java/com/slippBoardQna/SlippQnaApplication.java
@@ -0,0 +1,13 @@
+package com.slippBoardQna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlippQnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlippQnaApplication.class, args);
+	}
+
+}
