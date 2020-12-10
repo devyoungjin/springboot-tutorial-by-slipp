92320dbb705293dd5b925130162cd3ed22b8fe65
Youngjin Mo <dev.youngjinmo@gmail.com>
2020-12-10 17:29:34 +0900

    Spring-Data-JPA 추가

--- src/main/java/com/devandy/domain/User.java
+++ src/main/java/com/devandy/domain/User.java
@@ -1,14 +1,30 @@
 package com.devandy.domain;
 
-import lombok.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
 @ToString
 @Getter @Setter
 public class User {
 	
-	public String userId;
-	public String password;
-	public String name;
-	public String email;
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	@Column
+	private String password;
+	
+	@Column
+	private String name;
+	
+	@Column
+	private String email;
 	
 }
