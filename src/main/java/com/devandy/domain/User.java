c3c195f4d4411f872735d95d1e2a4569a19510fe
Youngjin Mo <dev.youngjinmo@gmail.com>
2020-12-10 16:11:20 +0900

    Lombok 적용

--- src/main/java/com/devandy/domain/User.java
+++ src/main/java/com/devandy/domain/User.java
@@ -1,34 +1,14 @@
 package com.devandy.domain;
 
-public class User {
-	public String userEmail;
-	public String userName;
-	public String userPassword;
-	
-	public String getUserEmail() {
-		return userEmail;
-	}
-	public void setUserEmail(String userEmail) {
-		this.userEmail = userEmail;
-	}
-	public String getUserName() {
-		return userName;
-	}
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-	public String getUserPassword() {
-		return userPassword;
-	}
-	public void setUserPassword(String userPassword) {
-		this.userPassword = userPassword;
-	}
-	
-	@Override
-	public String toString() {
-		return "user [userEmail=" + userEmail + ", userName=" + userName + ", userPassword=" + userPassword + "]";
-	}
-	
-	
+import lombok.*;
 
+@ToString
+@Getter @Setter
+public class User {
+	
+	public String userId;
+	public String password;
+	public String name;
+	public String email;
+	
 }
